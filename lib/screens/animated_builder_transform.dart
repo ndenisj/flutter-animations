bbabb9181c01e21106f3677d71786f9747842fb4
Denis John <denis.john.ndarake@gmail.com>
2023-02-02 19:15:55 +0100

    AnimatedBuilder and Transform Example

--- lib/screens/animated_builder_transform.dart
+++ lib/screens/animated_builder_transform.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show pi;
 
 class AnimatedBuilderAndTransform extends StatefulWidget {
   const AnimatedBuilderAndTransform({super.key});
@@ -9,11 +10,61 @@ class AnimatedBuilderAndTransform extends StatefulWidget {
 }
 
 class _AnimatedBuilderAndTransformState
-    extends State<AnimatedBuilderAndTransform> {
+    extends State<AnimatedBuilderAndTransform>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _animation = Tween<double>(
+      begin: 0.0,
+      end: 2 * pi,
+    ).animate(_controller);
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Center(
+        child: AnimatedBuilder(
+            animation: _controller,
+            builder: (context, child) {
+              return Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()..rotateY(_animation.value),
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: const Offset(0, 3),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            }),
+      ),
     );
   }
 }
