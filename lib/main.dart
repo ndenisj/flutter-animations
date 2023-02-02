0eb7b17d31df7004643df28971167f3c26c602ec
Denis John <denis.john.ndarake@gmail.com>
2023-02-02 18:31:12 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animations/screens/animated_builder_transform.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Animations'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AnimatedBuilderAndTransform(),
+                  ),
+                );
+              },
+              child: Text("Animated Builder & Transform"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/animated_builder_transform.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AnimatedBuilderAndTransform extends StatefulWidget {
+  const AnimatedBuilderAndTransform({super.key});
+
+  @override
+  State<AnimatedBuilderAndTransform> createState() =>
+      _AnimatedBuilderAndTransformState();
+}
+
+class _AnimatedBuilderAndTransformState
+    extends State<AnimatedBuilderAndTransform> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
